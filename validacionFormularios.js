206a73329123f9d7bd95238189d63459f717756c
Ivan <ivancarrillovela@gmail.com>
2025-04-28 12:32:02 +0200

    Implementado javascript utilizando código js de bulma

--- validacionFormularios.js
+++ validacionFormularios.js
@@ -128,3 +128,17 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// BURGER NAVBAR
+
+$(document).ready(function() {
+
+  // Check for click events on the navbar burger icon
+  $(".navbar-burger").click(function() {
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      $(".navbar-burger").toggleClass("is-active");
+      $(".navbar-menu").toggleClass("is-active");
+
+  });
+});
