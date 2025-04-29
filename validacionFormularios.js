bb0dfb72a6bef82b9301018302f69bf04ae6c1ce
Ivan <ivancarrillovela@gmail.com>
2025-04-29 11:54:05 +0200

    Corregido código js y aplicada la funcionalidad tanto en el "index" como en "hazte voluntario"

--- validacionFormularios.js
+++ validacionFormularios.js
@@ -132,23 +132,23 @@ document.addEventListener("DOMContentLoaded", () => {
 // BURGER NAVBAR
 
 document.addEventListener('DOMContentLoaded', () => {
+  const $navbarBurgers = Array.from(document.querySelectorAll('.navbar-burger'));
 
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
 
-  // Add a click event on each of them
-  $navbarBurgers.forEach( el => {
-    el.addEventListener('click', () => {
-
-      // Get the target from the "data-target" attribute
-      const target = el.dataset.target;
-      const $target = document.getElementById(target);
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      el.classList.toggle('is-active');
-      $target.classList.toggle('is-active');
-
+        if ($target) {
+          el.classList.toggle('is-active');
+          $target.classList.toggle('is-active');
+        } else {
+          console.warn(`No se encontró un elemento con el ID ${target}`);
+        }
+      });
     });
-  });
-
+  } else {
+    console.warn("No se encontraron elementos con la clase navbar-burger");
+  }
 });
